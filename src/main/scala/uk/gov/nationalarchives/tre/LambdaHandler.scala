a0bcf74cdfd19cc342acc1e3b5ebb655e1d53889
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-12-04 14:45:37 +0000

    feat: Lamda to process eventbus monitoring queue

--- src/main/scala/uk/gov/nationalarchives/tre/LambdaHandler.scala
+++ src/main/scala/uk/gov/nationalarchives/tre/LambdaHandler.scala
@@ -3,14 +3,11 @@ package uk.gov.nationalarchives.tre
 import com.amazonaws.services.lambda.runtime.events.{LambdaDestinationEvent, SNSEvent, SQSEvent}
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
-class LambdaHandler[Event]() extends RequestHandler[Event, Unit] {
+class LambdaHandler() extends RequestHandler[SQSEvent, Unit] {
 
-  override def handleRequest(event: Event, context: Context): Unit = {
+  override def handleRequest(event: SQSEvent, context: Context): Unit = {
     event match {
-      case snsEvent: SNSEvent => SNSEventHandler.logEvent(snsEvent, context.getLogger)
-      case lambdaDestinationEvent: LambdaDestinationEvent => DestinationEventHandler.logEvent(lambdaDestinationEvent, context.getLogger)
-      case sqsEvent: SQSEvent => SQSEventHandler.logEvent(sqsEvent, context.getLogger)
-      case _=> throw new NotImplementedError(s"Unrecognised lambda event")
+      case sqsEvent: SQSEvent => sqsEvent.getRecords.forEach(record => context.getLogger.log(s"Record received: ${record.getBody}"))
     }
   }
 }
