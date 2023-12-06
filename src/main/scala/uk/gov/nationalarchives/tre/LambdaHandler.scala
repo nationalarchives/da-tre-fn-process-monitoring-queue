5d9f6c029c3298e24ac5c1fc0fea6754fa15a934
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-12-06 15:16:59 +0000

    Add notification implementation

--- src/main/scala/uk/gov/nationalarchives/tre/LambdaHandler.scala
+++ src/main/scala/uk/gov/nationalarchives/tre/LambdaHandler.scala
@@ -2,25 +2,25 @@ package uk.gov.nationalarchives.tre
 
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import org.apache.http.impl.client.HttpClients
+import uk.gov.nationalarchives.tre.MessageParsingUtils.parseStringMap
 
 import scala.sys.env
-import scala.util.Try
 
 class LambdaHandler() extends RequestHandler[ScheduledEvent, Unit] {
 
   override def handleRequest(event: ScheduledEvent, context: Context): Unit = {
-    context.getLogger.log(s"Logging successfully.")
-    context.getLogger.log(s"Scheduled event received: $event")
     val monitoringQueueArn = env("MONITORING_QUEUE_ARN")
     val monitoringQueueUrl = SQSUtils.deriveQueueUrl(monitoringQueueArn)
-    Try(SQSUtils.receiveAllMessages(monitoringQueueUrl))
-      .fold(
-        t => context.getLogger.log(s"Something went wrong here: ${t.toString}"),
-        messages => {
-          messages.foreach(m => context.getLogger.log(s"Received message: ${m.body()} \n"))
-          SQSUtils.batchDeleteMessages(monitoringQueueUrl, messages)
-          SQSUtils.closeClient()
-        }
-      )
+    val messages = SQSUtils.receiveAllMessages(monitoringQueueUrl)
+
+    val slackEndpoints = parseStringMap(env("NOTIFIABLE_SLACK_MONITORING_ENDPOINTS"))
+    val httpClient = HttpClients.createDefault()
+    val slackUtils = new SlackUtils(httpClient)
+    val messageText = SlackMessageBuilder.buildSlackMessage(SlackMessageBuilder.matchMessages(messages.map(_.body())))
+    
+    slackEndpoints.foreach { case (c, wh) => slackUtils.postMessage(wh, messageText, c, "monitoring-lambda") }
+    SQSUtils.batchDeleteMessages(monitoringQueueUrl, messages)
+    SQSUtils.closeClient()
   }
 }
---
+++ src/main/scala/uk/gov/nationalarchives/tre/SlackMessageBuilder.scala
@@ -0,0 +1,54 @@
+package uk.gov.nationalarchives.tre
+
+import uk.gov.nationalarchives.common.messages.Producer
+import uk.gov.nationalarchives.common.messages.Status.COURT_DOCUMENT_PARSE_WITH_ERRORS
+import uk.gov.nationalarchives.da.messages.bag.available.BagAvailable
+import uk.gov.nationalarchives.da.messages.courtdocumentpackage.available.CourtDocumentPackageAvailable
+import uk.gov.nationalarchives.da.messages.courtdocumentpackage.available.Status.COURT_DOCUMENT_PARSE_NO_ERRORS
+import uk.gov.nationalarchives.da.messages.request.courtdocument.parse.RequestCourtDocumentParse
+import uk.gov.nationalarchives.tre.MessageParsingUtils.{parseBagAvailable, parseCourtDocumentPackageAvailable, parseGenericMessage, parseRequestCourtDocumentParse}
+
+object SlackMessageBuilder {
+  def matchMessages(messageBodies: Seq[String]): Seq[MatchedMessages] = {
+    val messagesWithMessageTypeAndProducer = messageBodies.map { m => 
+      val properties = parseGenericMessage(m).properties
+      (m, properties.messageType, properties.producer)
+    }
+    val bagAvailableMessages = messagesWithMessageTypeAndProducer.collect { 
+      case (message, "uk.gov.nationalarchives.da.messages.bag.available.BagAvailable", _) => parseBagAvailable(message)
+    }
+    val requestCourtDocumentParseMessages = messagesWithMessageTypeAndProducer.collect {
+      case (message, "uk.gov.nationalarchives.da.messages.request.courtdocument.parse.RequestCourtDocumentParse", Producer.FCL) => 
+        parseRequestCourtDocumentParse(message)
+    }
+    val courtDocumentPackageAvailableMessages = messagesWithMessageTypeAndProducer.collect {
+      case (message, "uk.gov.nationalarchives.da.messages.courtdocumentpackage.available.CourtDocumentPackageAvailable", _) =>          
+        parseCourtDocumentPackageAvailable(message)
+    }
+    requestCourtDocumentParseMessages.map(rcdp => MatchedMessages(
+      Right(rcdp), courtDocumentPackageAvailableMessages.find(_.parameters.reference == rcdp.parameters.reference))
+    ) ++ bagAvailableMessages.map(ba => MatchedMessages(
+      Left(ba), courtDocumentPackageAvailableMessages.find(_.parameters.reference == ba.parameters.reference))
+    )
+  }
+  
+  def buildSlackMessage(matchedMessages: Seq[MatchedMessages]): String = {
+    val matchedWithNoErrors = matchedMessages.filter(_.messageOut.exists(_.parameters.status == COURT_DOCUMENT_PARSE_NO_ERRORS))
+    val matchedWithErrors = matchedMessages.filter(_.messageOut.exists(_.parameters.status == COURT_DOCUMENT_PARSE_WITH_ERRORS))
+    val unmatched = matchedMessages.filter(_.messageOut.isEmpty)
+    val noErrorsSummary = if (matchedWithNoErrors.nonEmpty) 
+      Some(s":tada: Processed ${matchedWithNoErrors.size} requests with no errors") 
+    else None
+    val matchedWithErrorsSummary = if (matchedWithErrors.nonEmpty) 
+      Some(s":warning: Processed ${matchedWithErrors.size} requests with errors, " +
+        s"references ${matchedWithErrors.map(_.messageIn.fold(_.parameters.reference, _.parameters.reference)).mkString(",")}")
+    else None
+    val unmatchedMessagesSummary = if (unmatched.nonEmpty) 
+      Some(s":interrobang: ${unmatched.size} requests found with no package available message, " +
+        s"references ${unmatched.map(_.messageIn.fold(_.parameters.reference, _.parameters.reference)).mkString(",")}")
+    else None
+    Seq(noErrorsSummary, matchedWithErrorsSummary, unmatchedMessagesSummary).flatten.mkString("\n") 
+  }
+}
+
+case class MatchedMessages(messageIn: Either[BagAvailable, RequestCourtDocumentParse], messageOut: Option[CourtDocumentPackageAvailable])
