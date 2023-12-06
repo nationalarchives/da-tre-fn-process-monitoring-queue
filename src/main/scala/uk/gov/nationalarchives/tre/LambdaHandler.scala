2fc5adb4a715c23179275ad830286283481b9b83
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-12-06 10:59:35 +0000

    Log scheduled event

--- src/main/scala/uk/gov/nationalarchives/tre/LambdaHandler.scala
+++ src/main/scala/uk/gov/nationalarchives/tre/LambdaHandler.scala
@@ -1,17 +1,12 @@
 package uk.gov.nationalarchives.tre
 
-import com.amazonaws.services.lambda.runtime.events.{LambdaDestinationEvent, SNSEvent, SQSEvent}
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
-class LambdaHandler() extends RequestHandler[SQSEvent, Unit] {
+class LambdaHandler() extends RequestHandler[ScheduledEvent, Unit] {
 
-  override def handleRequest(event: SQSEvent, context: Context): Unit = {
-    event match {
-      case sqsEvent: SQSEvent => {
-        context.getLogger.log(s"sqs event received: $sqsEvent")
-        sqsEvent.getRecords.forEach(record => context.getLogger.log(s"Record received: ${record.getBody}"))
-      }
-      case _ => context.getLogger.log("Did not receive an SQS event") 
-    }
+  override def handleRequest(event: ScheduledEvent, context: Context): Unit = {
+    context.getLogger.log(s"Logging successfully.")
+    context.getLogger.log(s"Scheduled event received: $event")
   }
 }
